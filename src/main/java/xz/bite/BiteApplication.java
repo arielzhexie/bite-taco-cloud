d770ce2caf716ba2ef88c60e78d469f6b37919d4
Ariel Zhe Xie <zhe.xie@blablacar.com>
2021-02-04 22:03:48 +0100

    initialize taco-cloud spring project

---
+++ src/main/java/xz/bite/BiteApplication.java
@@ -0,0 +1,13 @@
+package xz.bite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xz/bite/BiteApplicationTests.java
@@ -0,0 +1,13 @@
+package xz.bite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
